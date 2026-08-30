cc85daf3f44b72f6049791f916c6db1ea3715484
suhail khan <sk72345654321@gmail.com>
2026-08-30 11:38:30 +0500

    commit changes

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ let folderMeta = {
         artist: "yousaf"
     },
     "jihad": {
-        image: "https://th.bing.com/th/id/OIP.qDwK7Uxkaa5rKj7Nc4i-CQHaEG?w=280&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
+        image: "https://tse4.mm.bing.net/th/id/OIP.qDwK7Uxkaa5rKj7Nc4i-CQHaEG?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
         artist: "yousaf"
     },
     "Rooh ki ghiza": {
